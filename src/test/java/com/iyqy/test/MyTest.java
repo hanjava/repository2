84b297284d6e3a72e5467fd60023f59249b3da4b
han_java <han_1990yyp@163.com>
2019-02-22 21:04:04 +0800

    update my_pro

--- src/test/java/com/iyqy/test/MyTest.java
+++ src/test/java/com/iyqy/test/MyTest.java
@@ -4,5 +4,11 @@ public class MyTest {
 
     public static void main(String[] args) {
         System.out.println("hello world.");
+        test();
+    }
+
+    public static void test(){
+        int i = 0;
+        System.out.println("i++ = " + i++);
     }
 }
